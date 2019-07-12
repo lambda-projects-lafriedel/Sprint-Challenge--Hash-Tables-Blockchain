297664274d25a88b15ebbfcfb39a38af1d595dda
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-12 12:12:53 -0400

    Ex1 tests pass, had to alter print_answer function

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -11,13 +11,42 @@ def get_indices_of_item_weights(weights, length, limit):
 
     """
     YOUR CODE HERE
+    # Example input: get_indices_of_item_weights([4,6,10,15,16], 5, 21)
+    # Example output: [3,1] (Indices of the weights 15 and 6 -- which equals limit of 21)
+    # Find two items whose sum of weights equals the param 'limit' and return their indices
+
+    # Higher valued index from weights list should be placed in index 0 and lower valued index from weights list placed in index 1
+    # Return a call to print_answer with the two indices
     """
 
-    return None
+    # for each item in weights array, insert it into the hash table. key = value from array, value = its index from weights array
+    if length > 1:
+
+        for i in range(len(weights)):
+            # (4, 0) (6,1) (10,2) (15,3) (16,4)
+            hash_table_insert(ht, weights[i], i)
+            # subtract length - weights[i] (21 - 4 = 17)
+        
+        for i in range(len(weights)):
+            diff = limit - weights[i]
+            # attempt to retrieve item from hash_table with key 17
+            match_index = hash_table_retrieve(ht, diff)
+
+            # if exists, the value is returned which is the list index
+            if match_index is not None:
+                print("COUPLE", weights[match_index], weights[i])
+                if match_index >= i:
+                    return print_answer([str(match_index), str(i)])
+                else:
+                    return print_answer([str(i), str(match_index)])
+    else:
+        return print_answer(None)
 
 
 def print_answer(answer):
-    if answer is None:
+    print("ANSWER", answer)
+    if answer is not None:
         print(str(answer[0] + " " + answer[1]))
+        return [int(answer[0]), int(answer[1])]
     else:
-        print("None")
+        print("None inside print answer")
