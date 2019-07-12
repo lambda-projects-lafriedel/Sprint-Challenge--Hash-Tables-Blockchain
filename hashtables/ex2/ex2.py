138d55835b98f74e70d47e07f318d4c99a1bbb16
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-12 13:24:58 -0400

    Short case flight hash table test passes

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,32 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
+    ht = HashTable(length)
     route = [None] * length
 
     """
     YOUR CODE HERE
     """
+    for ticket in tickets:
+        # insert each ticket into the hash table
+        hash_table_insert(ht, ticket.source, ticket.destination)
+
+    # retrieve the starting ticket, where the key is NONE
+    curr_ticket = hash_table_retrieve(ht, "NONE")
+    print("CURR TICKET", curr_ticket)
+
+    # add the starting ticket's "destination" (value) as the starting airport
+    route[0] = curr_ticket
+    i = 1
+    # while current ticket destination is not None
+    while route[-1] is not "NONE":
+        next_ticket = hash_table_retrieve(ht, curr_ticket)
+        print("NEXT TICKET", next_ticket)
+        # next_ticket should be PDX, DCA
+        route[i] = next_ticket
+        print("ROUTE", route)
+        # retrieve the next ticket that has the key of the current ticket as its value
+        curr_ticket = next_ticket
+        i += 1
 
     return route
